fe7c0b03a7d6d594f9fc96a2fcfd92508fb14640
Alondra1752 <alondrasant7739@gmail.com>
2024-09-11 08:53:10 -0500

    added apu for thought and user

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const thoughtController = require('../../controllers/thought-controller');
+
+router.route('/')
+  .get(thoughtController.getAllThoughts)
+  .post(thoughtController.createThought);
+
+router.route('/:id')
+  .get(thoughtController.getThoughtById)
+  .put(thoughtController.updateThought)
+  .delete(thoughtController.deleteThought);
+
+router.route('/:thoughtId/reactions')
+  .post(thoughtController.addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(thoughtController.removeReaction);
+
+module.exports = router;
+
